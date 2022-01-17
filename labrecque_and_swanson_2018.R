306bb3ab3394e84b8cdce602aebaf6a698c3f3d3
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-17 16:52:19 +0100

    Upload CSV

--- labrecque_and_swanson_2018.R
+++ labrecque_and_swanson_2018.R
@@ -107,3 +107,7 @@ row.names(ds) <- NULL
 names(ds)[1:2] <- c("GA shape", "Exposure window")
 names(ds)[names(ds) %in% c("rel30","rel50")] <- c("rel30 (%)", "rel50 (%)")
 ds
+
+
+# Added by RJCB
+write.csv(ds, "table_1.csv")
